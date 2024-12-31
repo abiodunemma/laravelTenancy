66d0a33291a4c71953a4d722b053460d8f99e4e9
abiodun emma <abiodunemma769@gmail.com>
2024-12-31 11:48:29 +0100

    first commit

---
+++ routes/tenant.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Support\Facades\Route;
+use App\Models\User;
+use Stancl\Tenancy\Middleware\InitializeTenancyByDomain;
+use Stancl\Tenancy\Middleware\PreventAccessFromCentralDomains;
+
+/*
+|--------------------------------------------------------------------------
+| Tenant Routes
+|--------------------------------------------------------------------------
+|
+| Here you can register the tenant routes for your application.
+| These routes are loaded by the TenantRouteServiceProvider.
+|
+| Feel free to customize them however you want. Good luck!
+|
+*/
+
+Route::middleware([
+    'web',
+    InitializeTenancyByDomain::class,
+    PreventAccessFromCentralDomains::class,
+])->group(function () {
+    Route::get('/', function () {
+        $users = User::get();
+        dd($users->toArray());
+
+        return 'This is your multi-tenant application. The id of the current tenant is ' . tenant('id');
+    });
+
+    Route::get('/test', function(){
+        return 'Hello'. tenant('id');
+    });
+});
