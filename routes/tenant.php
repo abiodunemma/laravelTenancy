37591af20589eabc449e6f24824423bdd4099735
abiodun emma <abiodunemma769@gmail.com>
2025-01-03 00:58:28 +0100

    final push

--- routes/tenant.php
+++ routes/tenant.php
@@ -30,11 +30,11 @@ Route::middleware([
 
         return 'This is your multi-tenant application. The id of the current tenant is ' . tenant('id');
     });
-    Route::get('/test', function(){
-        return 'Hello' .tenant('id');
-    });
-
-    // Route::get('/login', function(){
-    //     return 'Hello'. tenant('id');
+    // Route::get('/test', function(){
+    //     return 'Hello' .tenant('id');
     // });
+
+    Route::get('/login', function(){
+        return 'Hello'. tenant('id');
+    });
 });
